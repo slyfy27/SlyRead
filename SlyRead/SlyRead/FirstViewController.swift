f742701a74e2da29596785da13bca58eb7e0c0b4
wushuying <411522457@qq.com>
2017-12-26 15:49:08 +0800

    Project initialization

---
+++ SlyRead/SlyRead/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SlyRead
+//
+//  Created by wushuying on 2017/12/26.
+//  Copyright © 2017年 wushuying. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
